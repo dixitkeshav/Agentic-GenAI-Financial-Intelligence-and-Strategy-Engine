b1c848fccd6007cfe3c5d2a8e27450f4b9939267
Keshav Dixit <dixitkeshav@users.noreply.github.com>
2026-02-11 01:37:08 +0530

    feat(config): update ASGI config

--- project-root/backend/config/asgi.py
+++ project-root/backend/config/asgi.py
@@ -1,16 +1,16 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+try:
+    from channels.routing import ProtocolTypeRouter, URLRouter
+    from channels.auth import AuthMiddlewareStack
+    from fetch_news import routing as fetch_news_routing
+    application = ProtocolTypeRouter({
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(URLRouter(fetch_news_routing.websocket_urlpatterns)),
+    })
+except ImportError:
+    application = django_asgi_app
